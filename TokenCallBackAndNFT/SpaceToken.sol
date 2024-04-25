00de11f239d96cf0c2f0ba02810a43686d79a5b7
David9199 <167073652+David9199@users.noreply.github.com>
2024-04-25 21:35:03 +0800

    Update SpaceToken.sol

--- TokenCallBackAndNFT/SpaceToken.sol
+++ TokenCallBackAndNFT/SpaceToken.sol
@@ -4,44 +4,23 @@ pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
+interface ICallBackContract {
+    function tokensReceived(address from, uint256 amount, bytes memory data) external returns (bool);
+}
+
 contract SpaceToken is ERC20 {
     constructor() ERC20("Space Token", "SPACE") {
         _mint(address(msg.sender), 1000000000000000000000 * 10**18);
     }
 
-    function transfer(address to, uint256 amount) public override  returns (bool) {
+    function transferCallBack(address to, uint256 amount, bytes memory data) public returns (bool) {
         require(amount > 0, "Transfer amount must be greater than zero");
 
         super.transfer(to, amount);
 
         if (isContract(to)) {
-            bytes memory methodData = abi.encodeWithSignature(
-                "tokensReceived(address,uint256)",
-                msg.sender,
-                amount
-            );
-            (bool success, ) = to.call(methodData);
-            require(success, "token received failed");
-        }
-
-        return true;
-    }
-
-    function transferForBuyNFT(address to, uint256 amount, address nftAddress, uint256 tokenId) public returns (bool) {
-        require(amount > 0, "Transfer amount must be greater than zero");
-
-        super.transfer(to, amount);
-
-        if (isContract(to)) {
-            bytes memory methodData = abi.encodeWithSignature(
-                "tokensReceived(address,uint256,address,uint256)",
-                msg.sender,
-                amount,
-                nftAddress,
-                tokenId
-            );
-            (bool success, ) = to.call(methodData);
-            require(success, "token received failed");
+            bool success = ICallBackContract(to).tokensReceived(msg.sender, amount, data);
+            require(success, "Call back failed");
         }
 
         return true;
